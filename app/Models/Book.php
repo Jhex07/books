0447cf8870f52f7b90f5be8a611f9dfd71ad321b
Jhex07 <jhonalejandro25montes@gmail.com>
2023-12-21 09:38:12 -0500

    Updated

---
+++ app/Models/Book.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'author_id',
+        'title',
+        'stock',
+        'description'
+    ];
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'number_id' => '1193099858',
+            'name' => 'Jhon',
+            'last_name' => 'Montes',
+            'email' => 'jhonalejandro25montes@gmail.com',
+            'password' => bcrypt(123456789),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
